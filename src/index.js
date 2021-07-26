068d8544ab65f165c980e9cd9c0340701140bfcc
h4rb4rd <75742825+h4rb4rd@users.noreply.github.com>
2021-07-27 01:55:58 +0300

    it's done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const expString = expr.replace( /\s/g, "");
+
+  let count = 0;
+  for (const value of expString) {
+    if (value === '(') count++;
+    if (value === ')') count--;
+  }
+
+  if (count) throw new Error('ExpressionError: Brackets must be paired');
+  if (expString.includes('/0')) throw new Error('TypeError: Division by zero.');
+    
+  return new Function(`return ${expr}`)();
 }
 
 module.exports = {
